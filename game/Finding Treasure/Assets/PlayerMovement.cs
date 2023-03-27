27fc5432b3efeecce7036643409455ae93bc71e2
Alina Fisenko <alina.fisenko.work@gmail.com>
2023-03-27 16:43:34 +0300

    PGS-13 #comment write movement script #27
    Added PlayerMovement script file

--- game/Finding Treasure/Assets/PlayerMovement.cs
+++ game/Finding Treasure/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello");
     }
 
     // Update is called once per frame
