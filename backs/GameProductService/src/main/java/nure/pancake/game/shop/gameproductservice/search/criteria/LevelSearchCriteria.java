cf544d53f68b9c4fd4f08a2c3a0df91e675e29c8
mxkata <mxkata0@gmail.com>
2023-03-28 18:24:23 +0300

    PGS-20 #comment I completed task making repository level #41 #close

---
+++ backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/search/criteria/LevelSearchCriteria.java
@@ -0,0 +1,63 @@
+package nure.pancake.game.shop.gameproductservice.search.criteria;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.*;
+import nure.pancake.game.shop.gameproductservice.entity.Level;
+import nure.pancake.game.shop.gameproductservice.utils.Range;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LevelSearchCriteria implements Specification<Level> {
+    @Singular(ignoreNullCollections = true)
+    private Collection<Long> levelIds;
+    @Singular(ignoreNullCollections = true)
+    private Collection<Long> gameIds;
+    private Range<Float> price;
+    private String name;
+    private Boolean hidden;
+
+    @Override
+    public Predicate toPredicate(Root<Level> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (!CollectionUtils.isEmpty(levelIds)) {
+            criteria.add(cb.in(root.get("id")).value(levelIds));
+        }
+        if (!CollectionUtils.isEmpty(gameIds)) {
+            criteria.add(cb.in(root.get("gameId")).value(gameIds));
+        }
+        if (StringUtils.hasText(name)) {
+            criteria.add(cb.like(root.get("name"), "%" + name + "%"));
+        }
+        if (Range.hasFrom(price)) {
+            criteria.add(
+                    cb.ge(root.get("price"), price.getFrom())
+            );
+        }
+        if (Range.hasTo(price)) {
+            criteria.add(
+                    cb.le(root.get("price"), price.getTo())
+            );
+        }
+        if (hidden != null) {
+            criteria.add(cb.equal(root.get("hidden"), hidden));
+        }
+        if (CollectionUtils.isEmpty(criteria)) {
+            return Specification.where((Specification<Level>) null).toPredicate(root, query, cb);
+        }
+        return cb.and(criteria.toArray(Predicate[]::new));
+    }
+}
