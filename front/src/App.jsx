194963fdf6e8bca03d90a27337090344b536e32c
ghospadi <Lolr2223@gmail.com>
2023-03-29 19:17:40 +0300

    PGS-39-Add-redirection-functionals-for-routs

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,6 @@
 import GameRoutes from "./routes";
 import BasketModal from "./components/UI/Modals/BasketModal";
-// import { useSelector } from "react-redux";
-// import { useEffect } from "react";
 function App() {
-  // const user = useSelector(state => state);
-
-  // useEffect(() => {
-  //   console.log(user);
-  // }, [user]);
-
   return (
     <>
       <BasketModal />
