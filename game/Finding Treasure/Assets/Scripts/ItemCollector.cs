1bb6c26855d23b8ce5e5f4b24608631ef5c42493
Alina Fisenko <alina.fisenko.work@gmail.com>
2023-03-29 22:54:47 +0300

    PGS-47 #comment added sound effects #close #63

--- game/Finding Treasure/Assets/Scripts/ItemCollector.cs
+++ game/Finding Treasure/Assets/Scripts/ItemCollector.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     [SerializeField] private Text cherriesText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
     private int cherries = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
+            collectionSoundEffect.Play();
             Destroy(collision.gameObject);
             cherries++;
             cherriesText.text = "Cherries: " + cherries;
