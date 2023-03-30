4245aca88b5b9348f6b187f9d46b7e40a431c9df
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-03-30 22:51:35 +0300

    PGS-30 #comment added login exception  #67

---
+++ backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/exceptions/LoginException.java
@@ -0,0 +1,8 @@
+package nure.pancake.game.shop.authorizationservice.exceptions;
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) {
+        super(message);
+    }
+
+}
