24a7834134b3ee6b1bb409f2e0413d5619579609
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-03-30 16:09:00 +0300

    PGS-29 #comment added configs and update pom #close #65

--- backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/AuthorizationServiceApplication.java
+++ backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/AuthorizationServiceApplication.java
@@ -2,6 +2,8 @@ package nure.pancake.game.shop.authorizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
