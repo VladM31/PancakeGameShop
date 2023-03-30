f27064115f1c40ab4f99930617f83d34d8d75b33
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-03-30 22:56:53 +0300

    PGS-30 #comment added and updated properties #67

--- backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/AuthorizationServiceApplication.java
+++ backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/AuthorizationServiceApplication.java
@@ -2,11 +2,14 @@ package nure.pancake.game.shop.authorizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = UserDetailsServiceAutoConfiguration.class
+)
 @EnableCaching
 public class AuthorizationServiceApplication {
 
