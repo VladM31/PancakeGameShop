fb55401845e01d5700222218d6a602d481b8256b
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-03-31 18:46:10 +0300

    PGS-31 #comment added and updated auth exceptions #71

---
+++ backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/exceptions/AuthException.java
@@ -0,0 +1,19 @@
+package nure.pancake.game.shop.authorizationservice.exceptions;
+
+public abstract class AuthException extends RuntimeException {
+
+    public AuthException() {
+    }
+
+    public AuthException(String message) {
+        super(message);
+    }
+
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthException(Throwable cause) {
+        super(cause);
+    }
+}
--- backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/exceptions/LoginException.java
+++ backs/AuthorizationService/src/main/java/nure/pancake/game/shop/authorizationservice/exceptions/LoginException.java
@@ -1,8 +1,18 @@
 package nure.pancake.game.shop.authorizationservice.exceptions;
 
-public class LoginException extends RuntimeException {
+public class LoginException extends AuthException {
+    public LoginException() {
+    }
+
     public LoginException(String message) {
         super(message);
     }
 
+    public LoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LoginException(Throwable cause) {
+        super(cause);
+    }
 }
