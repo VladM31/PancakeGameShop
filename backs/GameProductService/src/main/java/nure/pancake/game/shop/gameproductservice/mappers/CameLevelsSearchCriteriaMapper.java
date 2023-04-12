149e91dea7b15b29b0374a1395581ff04966c75b
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-04-12 19:43:17 +0300

    PGS-79 #comment added GameLevelsSearchCriteria Mapper #close  #109

---
+++ backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/mappers/CameLevelsSearchCriteriaMapper.java
@@ -0,0 +1,39 @@
+package nure.pancake.game.shop.gameproductservice.mappers;
+
+import nure.pancake.game.shop.gameproductservice.filters.GameLevelsFilter;
+import nure.pancake.game.shop.gameproductservice.search.criteria.GameSearchCriteria;
+import nure.pancake.game.shop.gameproductservice.search.criteria.LevelSearchCriteria;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CameLevelsSearchCriteriaMapper {
+    private final ModelMapper mapper;
+
+    public CameLevelsSearchCriteriaMapper() {
+        this.mapper = new ModelMapper();
+        mapFrom();
+    }
+
+    private void mapFrom(){
+        TypeMap<GameLevelsFilter, GameSearchCriteria> propertyMapperFrom =
+                this.mapper.createTypeMap(GameLevelsFilter.class, GameSearchCriteria.class);
+
+        propertyMapperFrom.addMappings(
+                vc -> vc.map(GameLevelsFilter::getGenres,GameSearchCriteria::setGenreNames)
+        );
+
+    }
+
+    public GameSearchCriteria toGameSearchCriteria(GameLevelsFilter filter){
+        return mapper.map(filter, GameSearchCriteria.class);
+    }
+
+    public LevelSearchCriteria toLevelSearchCriteria(GameLevelsFilter f){
+        return LevelSearchCriteria.builder()
+                .levelIds(f.getLevelIds())
+                .gameIds(f.getGameIds())
+                .build();
+    }
+}
