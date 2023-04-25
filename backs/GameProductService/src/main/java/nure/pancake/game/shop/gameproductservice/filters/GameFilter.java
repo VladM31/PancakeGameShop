d63b71139c38d026ae8256e15df7673bd13cd838
mxkata <mxkata0@gmail.com>
2023-04-25 20:32:32 +0300

    PGS-73 #comment I completed task making a filter for games and mapper to SearchCriteria. #147 #close

---
+++ backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/filters/GameFilter.java
@@ -0,0 +1,26 @@
+package nure.pancake.game.shop.gameproductservice.filters;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import nure.pancake.game.shop.gameproductservice.dataobjects.sortfiled.GameSortFiled;
+import nure.pancake.game.shop.gameproductservice.utils.Range;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class GameFilter extends FilterPage<GameSortFiled> {
+    @Singular(ignoreNullCollections = true)
+    private Collection<Long> gameIds;
+    @Singular(ignoreNullCollections = true)
+    private Collection<String> genres;
+    private Range<Float> price;
+    private Range<Float> ageRating;
+    private Range<LocalDate> releaseDate;
+    private String name;
+
+}
