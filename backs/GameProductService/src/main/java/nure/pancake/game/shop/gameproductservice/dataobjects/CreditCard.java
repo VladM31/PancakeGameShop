1868a85bf6b26cf9cb144ba1555981701d58cef3
vladyslav.mormu <vladyslav.mormul@nure.ua>
2023-04-29 13:33:58 +0300

    PGS-92 #comment update CreditCard #172

--- backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/dataobjects/CreditCard.java
+++ backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/dataobjects/CreditCard.java
@@ -4,16 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreditCard {
     private String cardNumber;
-    private LocalDate expiryDate;
+    private String expiryDate;
     private String cardName;
     private String cvv2;
 }
