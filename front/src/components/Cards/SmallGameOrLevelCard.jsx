dd4398ce64c76e60c16442e807d207888f1e1ddb
Ghospadi <Lolr2223@gmail.com>
2023-05-02 21:36:25 +0300

    PGS-89 #comment SmallGameOrLevelCard fixes

--- front/src/components/Cards/SmallGameOrLevelCard.jsx
+++ front/src/components/Cards/SmallGameOrLevelCard.jsx
@@ -4,7 +4,7 @@ import {styled} from '@mui/system';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import {Link} from 'react-router-dom';
-import {useNavigate} from "react-router";
+import {useNavigate} from 'react-router';
 
 function SmallGameOrLevelCard({
                                   gameId,
@@ -16,62 +16,56 @@ function SmallGameOrLevelCard({
                                   cardType, // Pass 'cart', 'library', 'gamepage' or any other type as a prop
                                   onButtonClick,
                                   showCards,
-                                  handleClose
+                                  handleClose,
+                                  extraCardContent, // Add this prop for extra content
                               }) {
-    const navigate = useNavigate()
-    const StyledCard = styled(Card)(({theme}) => isLibraryLevelCard ?
-        ({
-            backgroundColor: '#B55D9C',
-            borderRadius: '15px',
-            width: '950px',
-            height: '200px',
-            scale: '0.8',
-        }) : (
-            {
-                backgroundColor: '#B55D9C',
-                borderRadius: '15px',
-                width: '950px',
-                height: '200px',
-            }));
+    const navigate = useNavigate();
+    const StyledCard = styled(Card)(({theme}) => ({
+        backgroundColor: '#B55D9C',
+        borderRadius: '15px',
+        width: '950px',
+        height: '200px',
+    }));
 
     const isLibraryCard = cardType === 'library';
-    const isLibraryLevelCard = cardType === 'libraryLevel';
     const isGamePageCard = cardType === 'gamepage';
     const isCartCard = cardType === 'cart';
 
     const navigationToGameOrLevel = (e, gameId, levelId) => {
         if (levelId) {
-            navigate(`/game/${gameId}/level/${levelId}`)
+            navigate(`/game/${gameId}/level/${levelId}`);
         } else {
-            navigate(`/game/${gameId}`)
+            navigate(`/game/${gameId}`);
         }
         handleClose();
         e.stopPropagation();
-    }
+    };
 
     const removeHandler = (e) => {
         e.stopPropagation();
         onButtonClick();
-    }
+    };
 
     return (
-        <StyledCard onClick={isCartCard ? e => navigationToGameOrLevel(e, gameId, levelId) : null}
-                    sx={isCartCard ? {cursor: 'pointer', marginBottom: '20px'} : {marginBottom: '20px'}}>
+        <StyledCard
+            onClick={isCartCard ? (e) => navigationToGameOrLevel(e, gameId, levelId) : null}
+            sx={isCartCard ? {cursor: 'pointer', marginBottom: '20px'} : {marginBottom: '20px'}}
+        >
             <CardContent sx={{display: 'flex', flexDirection: 'row'}}>
                 <CardMedia component="img" sx={{borderRadius: '20px', width: '35%'}} height="170" image={mainImage}
                            alt={name}/>
                 <CardContent
                     sx={{display: 'flex', width: '65%', justifyContent: 'space-between', alignItems: 'center'}}>
-                    <CardContent sx={isLibraryCard || isLibraryLevelCard ? {
-                        display: 'flex',
-                        height: '150px',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between'
-                    } : null}>
-                        <Typography variant={isLibraryCard || isLibraryLevelCard ? 'h5' : 'h4'} color="white"
-                                    sx={isLibraryLevelCard ? {mt: 1} : {textAlign: 'center', mt: 1}}>{name}</Typography>
-                        {purchasedDate &&
-                            <Typography variant="h6" color="white" sx={{mt: 1}}>Придбано: {purchasedDate.slice(0, 10)}</Typography>}
+                    <CardContent>
+                        <Typography variant="h4" color="white" sx={{textAlign: 'center', mt: 1}}>
+                            {name}
+                        </Typography>
+                        {purchasedDate && (
+                            <Typography variant="h6" color="white" sx={{mt: 1}}>
+                                Придбано: {purchasedDate.slice(0, 10)}
+                            </Typography>
+                        )}
+                        {extraCardContent && extraCardContent}
                     </CardContent>
                     <CardContent sx={{
                         display: 'flex',
@@ -80,27 +74,24 @@ function SmallGameOrLevelCard({
                         justifyContent: 'space-between',
                         p: 2
                     }}>
-                        {
-                            isLibraryCard || isLibraryLevelCard ? (
-                                <Link style={{color: '#fff', marginTop: '15px'}}
-                                      to={isLibraryLevelCard ? `/game/${gameId}/level/${levelId}` : `/game/${gameId}`}>
-                                    Переглянути
-                                </Link>
-                            ) : (
-                                <Typography variant="h5" color="white">Ціна {price}$</Typography>
-                            )
-                        }
-                        {
-                            isLibraryCard ? (
-                                <Button onClick={onButtonClick} color='inherit' variant="contained">
-                                    {showCards ? (<ArrowUpwardIcon/>) : (<ArrowDownwardIcon/>)}
-                                </Button>
-                            ) : (
-                                isLibraryLevelCard ? null : !isGamePageCard ? <Button variant="contained" color="error"
-                                                                                      onClick={(e) => removeHandler(e)}>Видалити</Button> :
-                                    <Button variant="contained" color="inherit">В корзину</Button>
-                            )
-                        }
+                        {isLibraryCard ? (
+                            <Link style={{color: '#fff', marginTop: '15px'}} to={`/game/${gameId}`}>
+                                Переглянути
+                            </Link>
+                        ) : (
+                            <Typography variant="h5" color="white">Ціна {price}$</Typography>
+                        )}
+                        {isLibraryCard ? (
+                            <Button onClick={onButtonClick} color="inherit" variant="contained">
+                                {showCards ? <ArrowUpwardIcon/> : <ArrowDownwardIcon/>}
+                            </Button>
+                        ) : !isGamePageCard ? (
+                            <Button variant="contained" color="error" onClick={(e) => removeHandler(e)}>
+                                Видалити
+                            </Button>
+                        ) : (
+                            <Button variant="contained" color="inherit">В корзину</Button>
+                        )}
                     </CardContent>
                 </CardContent>
             </CardContent>
