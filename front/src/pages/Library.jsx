96fafb888d149059daf0fc764404c13a4b5abd8c
Ghospadi <Lolr2223@gmail.com>
2023-05-02 21:41:24 +0300

    PGS-89 #comment library additional condition

--- front/src/pages/Library.jsx
+++ front/src/pages/Library.jsx
@@ -18,11 +18,15 @@ function Library() {
 
     return (
         <Box>
-            <Typography>Бібліотека ігор</Typography>
+            <Typography variant={'h3'} color={'white'}>Бібліотека ігор</Typography>
             <Box sx={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-                {boughtGames.map((game) => (
-                    <CardsBox key={game.id} game={game} />
-                ))}
+                {
+                    boughtGames.length > 0 ? boughtGames.map((game) => (
+                        <CardsBox key={game.id} game={game}/>
+                    )) : <Box
+                        sx={{display: 'flex', alignItems: 'center', height: 500, fontSize: '24px', color: 'white'}}>Ви
+                        ще не купили жодної гри!</Box>
+                }
             </Box>
         </Box>
     )
