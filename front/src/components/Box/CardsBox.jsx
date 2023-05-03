43ae9fb783c5265c579144ec73431145dec67c14
Ghospadi <Lolr2223@gmail.com>
2023-05-03 22:11:03 +0300

    PGS-104 #comment Library if no levels bought layout

--- front/src/components/Box/CardsBox.jsx
+++ front/src/components/Box/CardsBox.jsx
@@ -5,17 +5,16 @@ import SmallLevelCard from "../Cards/SmallLevelCard";
 
 const CardsBox = ({game}) => {
     const [isShown, setIsShown] = useState(false);
-
     return (
         <Box>
             <SmallGameCard {...game} isShown={isShown} showHandler={() => setIsShown((prevState) => !prevState)}/>
-            {isShown ? game.levels.map((level) => (
-                <SmallLevelCard gamesId={game.gamesId} key={level.levelsId} {...level}/>
-            )) : (
-                <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px'}}>
-                    <Typography variant={'h2'} color={'white'}>Немає куплених рівнів</Typography>
-                </Box>
-            )}
+            {isShown ? game && game.levels.length > 0 ? game.levels.map((level) => (
+                    <SmallLevelCard gamesId={game.gamesId} key={level.levelsId} {...level}/>
+                )) : (<Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px'}}>
+                    <Typography variant={'h4'} color={'white'}>Немає куплених рівнів</Typography>
+                </Box>) :
+                null
+            }
         </Box>
     );
 };
