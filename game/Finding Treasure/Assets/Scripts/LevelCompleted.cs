adff878ab02618bb4067c532edceb2367d95a3ec
Alina Fisenko <alina.fisenko.work@gmail.com>
2023-05-16 23:00:49 +0300

    Fixed-bugs #close #206

--- game/Finding Treasure/Assets/Scripts/LevelCompleted.cs
+++ game/Finding Treasure/Assets/Scripts/LevelCompleted.cs
@@ -1,13 +1,58 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelCompleted : MonoBehaviour
 {
+    [SerializeField] private Text cherriesText;
+    private readonly Dictionary<long, long> keyValuePairs = new Dictionary<long, long>();
+
+    private void Start()
+    {
+        cherriesText.text = GlobalState.collectedCherries.ToString();
+    }
+    public LevelCompleted()
+    {
+        keyValuePairs.Add(2, 13);
+        keyValuePairs.Add(4, 14);
+        keyValuePairs.Add(6, 15);
+
+    }
+    public static bool Contains<T>(T[] array, T item)
+    {
+        List<T> list = new List<T>(array);
+        return list.Contains(item);
+    }
+
     public void NextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        long levelId = keyValuePairs[SceneManager.GetActiveScene().buildIndex + 1];
+
+        if (Contains(GlobalState.loginResult.user.purchasedLevels, levelId))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+        
+       
+        bool isStarted = false;
+
+        foreach (var item in keyValuePairs)
+        {
+            if (isStarted)
+            {
+                SceneManager.LoadScene(Convert.ToInt32(item.Key));
+                return;
+            }
+            if(item.Value == levelId)
+            {
+                isStarted = true;
+            }
+        }
+
+        SceneManager.LoadScene(1);
     }
 
     public void MainMenu()
