8fc2b985eb475dbbba2ed60e58ae63304d7f44b3
VladM31 <83096749+VladM31@users.noreply.github.com>
2023-05-16 23:52:11 +0300

    Revert "Fixed-bugs #close #206"

--- game/Finding Treasure/Assets/Scripts/PlayerLife.cs
+++ game/Finding Treasure/Assets/Scripts/PlayerLife.cs
@@ -7,101 +7,66 @@ using UnityEngine.SceneManagement;
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField] private AudioSource deathSoundEffect;
-    [SerializeField] private AudioSource damageSoundEffect;
 
     GameObject[] safeZones;
 
     private Animator anim;
     private Rigidbody2D rb;
 
-    public static int health;
-    [SerializeField] public GameObject Heart1, Heart2, Heart3;
-
     private void Start()
     {
         anim= GetComponent<Animator>();
         rb= GetComponent<Rigidbody2D>();
         safeZones = GameObject.FindGameObjectsWithTag("SafeZone");
-
-        health = 3;
     }
+
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.CompareTag("Trap"))
+    //    {
+    //        Die();
+    //    }
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject.tag == "Trap")
         {
-            health -= 1;
+            Health.health -= 1;
 
-            switch (health)
+            if (Health.health == 0)
+                Die();
+            else
             {
-                case 2:
-                    Destroy(Heart1);
-                    Hit();
-                    break;
+                GameObject nearestSafeZone = null;
+                float nearestDistance = Mathf.Infinity;
+                foreach (GameObject safeZone in safeZones)
+                {
+                    float distance = Vector3.Distance(transform.position, safeZone.transform.position);
+                    if (distance < nearestDistance)
+                    {
+                        nearestDistance = distance;
+                        nearestSafeZone = safeZone;
+                    }
+                }
 
-                case 1:
-                    Destroy(Heart2);
-                    Hit();
-                    break;
-
-                case 0:
-                    Destroy(Heart3);
-                    Die();
-                    return;
+                // перемещаем игрока в ближайшую безопасную зону
+                transform.position = nearestSafeZone.transform.position;
             }
-
-            //if (health == 0)
-            //{
-                
-            //}
-            //else
-            //{
-                TeleportToSafeZone();
-            //}
 
         }
     }
 
- 
     private void Die()
     {
         deathSoundEffect.Play();
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death");
-        Invoke("GetNextScene", 1.3f);
     }
 
-    private void GetNextScene()
+    private void RestartLevel()
     {
-        SceneManager.LoadScene("YouLost");
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
-    private void Hit()
-    {
-        damageSoundEffect.Play();        
-    }
-
-    private void TeleportToSafeZone()
-    {
-        GameObject nearestSafeZone = null;
-        float nearestDistance = Mathf.Infinity;
-        foreach (GameObject safeZone in safeZones)
-        {
-            float distance = Vector3.Distance(transform.position, safeZone.transform.position);
-            if (distance < nearestDistance)
-            {
-                nearestDistance = distance;
-                nearestSafeZone = safeZone;
-            }
-        }
-
-        // перемещаем игрока в ближайшую безопасную зону
-        transform.position = nearestSafeZone.transform.position;
-    }
-    //private void RestartLevel()
-    //{
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
 }
