35da6ee867e83861e9528c325e8006e49c611339
mxkata <mxkata0@gmail.com>
2023-05-20 14:11:29 +0300

    PGS-111 #comment I completed making PromoCodeSearchCriteria. #close #222

---
+++ backs/GameProductService/src/main/java/nure/pancake/game/shop/gameproductservice/search/criteria/PromoCodeSearchCriteria.java
@@ -0,0 +1,60 @@
+package nure.pancake.game.shop.gameproductservice.search.criteria;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.*;
+import nure.pancake.game.shop.gameproductservice.entities.PromoCodeEntity;
+import nure.pancake.game.shop.gameproductservice.utils.Range;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.CollectionUtils;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PromoCodeSearchCriteria implements Specification<PromoCodeEntity> {
+    @Singular(ignoreNullCollections = true)
+    private Collection<Long> promoIds;
+    private Range<Integer> discountPercentage;
+    private Range<LocalDate> endDate;
+
+    @Override
+    public Predicate toPredicate(Root<PromoCodeEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (!CollectionUtils.isEmpty(promoIds)) {
+            criteria.add(cb.in(root.get("id")).value(promoIds));
+        }
+        if (Range.hasFrom(discountPercentage)) {
+            criteria.add(
+                    cb.ge(root.get("discountPercentage"), discountPercentage.<Integer>fromMap(Integer::parseInt))
+            );
+        }
+        if (Range.hasTo(discountPercentage)) {
+            criteria.add(
+                    cb.le(root.get("discountPercentage"), discountPercentage.<Integer>toMap(Integer::parseInt))
+            );
+        }
+        if (Range.hasFrom(endDate)) {
+            criteria.add(
+                    cb.greaterThanOrEqualTo(root.get("endDate"), endDate.<LocalDate>fromMap(LocalDate::parse))
+            );
+        }
+        if (Range.hasTo(endDate)) {
+            criteria.add(
+                    cb.lessThanOrEqualTo(root.get("endDate"), endDate.<LocalDate>toMap(LocalDate::parse))
+            );
+        }
+        if (CollectionUtils.isEmpty(criteria)) {
+            return Specification.where((Specification<PromoCodeEntity>) null).toPredicate(root, query, cb);
+        }
+        return cb.and(criteria.toArray(Predicate[]::new));
+    }
+}
