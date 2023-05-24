5131ade6318bd8144b466b79334d9fdc9e6b9403
VladK0rzun <vlad2012759@gmail.com>
2023-05-24 22:48:22 +0300

    PGS-130 #comment create scene for pause menu #529 #close

---
+++ game/Kitten's adventure/Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    private bool pause = false;
+    public GameObject pauseMenu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pause) {
+                Resume();
+            } else if(pause)
+            {
+                MainMenu();
+            }
+            else
+            {
+                Payse();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        pause = false;
+    }
+
+    void Payse()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        pause = true;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start Screen 1");
+    }
+
+}
