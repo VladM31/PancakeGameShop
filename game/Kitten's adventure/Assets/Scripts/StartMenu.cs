47a75f24d513aa149a7fef9efca02de7cdafc930
VladK0rzun <vlad2012759@gmail.com>
2023-05-24 16:55:27 +0300

    PGS-121 #comment create second scene with start menu #253 #close

---
+++ game/Kitten's adventure/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    long[] result = GlobalState.loginResult.user.purchasedLevels;
+    
+    public void StartGame()
+    {
+        List<string> boughtLevels = new List<string>();
+        for (int i = 0; i < result.Length; i++)
+        {
+            Debug.Log(result[i]);
+            boughtLevels.Add("Level" + (result[i] - 21));
+            SceneManager.LoadScene(boughtLevels[0]);
+        }
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene("Levels"); 
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
