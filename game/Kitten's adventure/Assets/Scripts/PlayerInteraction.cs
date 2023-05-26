f2a2501a3edbfeedc1730db32e0581432928e053
Alina Fisenko <alina.fisenko.work@gmail.com>
2023-05-27 01:45:44 +0300

    PGS-163 #comment Added first dialogue of coffer  #280

--- game/Kitten's adventure/Assets/Scripts/PlayerInteraction.cs
+++ game/Kitten's adventure/Assets/Scripts/PlayerInteraction.cs
@@ -7,28 +7,53 @@ public class PlayerInteraction : MonoBehaviour
 {
     private bool isNearCoffer = false;
     public bool isPicked = false;
+    public GameObject text;
+
+    private bool isAlreadyPicked = false;
+
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManagerLeve2>().StartDialogue(dialogue);
+    }
+
+
+
+    private void Start()
+    {
+        text.SetActive(false);
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && isNearCoffer)
+        if (Input.GetKeyDown(KeyCode.E) && isNearCoffer &&!isAlreadyPicked)
         {
             Debug.Log("Hello");
             isPicked = true;
+            isAlreadyPicked = true;
+            text.SetActive(false);
+            TriggerDialogue();
         }
     }
+
     public void OnTriggerStay2D(Collider2D collision)
     {
-        if( collision.gameObject.CompareTag("Coffer"))
+        if( collision.gameObject.CompareTag("Coffer") && !isAlreadyPicked)
         {
             isNearCoffer = true;
+            text.SetActive(true);
 
         }
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Coffer"))
+        if (collision.gameObject.CompareTag("Coffer") && !isAlreadyPicked)
         {
             isNearCoffer = false;
+            text.SetActive(false);
+
         }
     }
 }
